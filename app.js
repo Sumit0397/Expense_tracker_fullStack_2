6ba6ba60dbe7e8c12451807b65280a9d1131274b
sumit majumdar <sumitmajumdar16@gmail.com>
2024-06-07 20:01:57 +0530

    Task 10: Premium Account Features

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const userRouter = require("./router/userRouter");
 const sequelize = require("./utils/database");
 const expenseRouter = require("./router/expenseRouter");
 const purchaseMembershipRouter = require("./router/purchaseMembershipRouter");
+const leaderboardRouter = require("./router/leaderboardRouter");
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 const User = require("./models/userModel");
 const Expense = require("./models/expenseModel");
@@ -22,6 +26,8 @@ app.use("/homepage" , expenseRouter);
 app.use("/expense" , expenseRouter);
 
 app.use("/purchase" , purchaseMembershipRouter);
+
+app.use("/premium", leaderboardRouter);
 
 // app.listen(3000 , () => {console.log("server is running in 3000 port")});
 
