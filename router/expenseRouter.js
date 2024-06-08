77792af786c1ffce104da00cd68f442f1a796a38
sumit majumdar <sumitmajumdar16@gmail.com>
2024-06-08 23:18:08 +0530

    Task 18: Paginating the expenses

--- router/expenseRouter.js
+++ router/expenseRouter.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const expenseController = require("../controllers/expenseController");
+const userAuthentication = require("../middleware/auth");
 
 router.use(express.static("public"));
 
 router.get("/", expenseController.getHomePage);
-router.get("/getAllExpenses", expenseController.getAllExpenses);
-router.delete("/deleteExpense/:id", expenseController.deleteExpense);
+router.get(
+  "/getAllExpenses",
+  userAuthentication,
+  expenseController.getAllExpenses
+);
+router.get(
+  "/getAllExpenses/:page",
+  userAuthentication,
+  expenseController.getAllExpensesforPagination
+);
+router.get(
+  "/deleteExpense/:id",
+  userAuthentication,
+  expenseController.deleteExpense
+);
 
-router.post("/addExpense", expenseController.addExpense);
-router.put("/editExpense/:id", expenseController.editExpense);
+router.post("/addExpense", userAuthentication, expenseController.addExpense);
+router.post(
+  "/editExpense/:id",
+  userAuthentication,
+  expenseController.editExpense
+);
 
 module.exports = router;
